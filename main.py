86958cb8a451c800fe792f258fc4fcd4188a61fa
Ardent-Mechanic <gikata.38@yandex.ru>
2021-04-23 23:48:41 +0300

    create main page

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    # arr = []
+    # for root, dirs, files in os.walk("./static/img/carusel_img"):
+    #     for filename in files:
+    #         arr.append(filename)
+    return render_template('test.html', link=url_for('static', filename='css/style.css'),)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8080, host='127.0.0.1')

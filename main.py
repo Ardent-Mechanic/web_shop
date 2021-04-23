e205c080dc32b6abc729287e5047c84b6c8059e3
Ardent-Mechanic <gikata.38@yandex.ru>
2021-04-24 01:33:58 +0300

    add new page - shipping_and_payment

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    # arr = []
-    # for root, dirs, files in os.walk("./static/img/carusel_img"):
-    #     for filename in files:
-    #         arr.append(filename)
-    return render_template('test.html', link=url_for('static', filename='css/style.css'),)
+@app.route('/main_page')
+def main():
+    link2 = ".." + url_for('static', filename='css/style.css')
+    return render_template('test.html', link=link2)
+
+@app.route('/ship&payment')
+def shipping_and_payment():
+    return render_template('shipping_and_payment.html')
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=5000, host='127.0.0.1')

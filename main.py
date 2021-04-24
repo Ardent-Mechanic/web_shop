f0f02e6f58df8bf0023ad31fb58a601baeeadde8
Ardent-Mechanic <gikata.38@yandex.ru>
2021-04-24 12:50:47 +0300

    add base.html

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ import os
 app = Flask(__name__)
 
 
+
 @app.route('/')
 @app.route('/main_page')
 def main():
     link2 = ".." + url_for('static', filename='css/style.css')
     return render_template('test.html', link=link2)
 
+
 @app.route('/ship&payment')
 def shipping_and_payment():
-    return render_template('shipping_and_payment.html')
+    return render_template('urn.html')
 
 
 if __name__ == '__main__':
